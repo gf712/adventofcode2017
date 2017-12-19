ee402dd3c46343793cf6533421fd9f63670c54e4
Gil <g.f.hoben@gmail.com>
2017-12-19 09:45:54 +0000

    added verbose option to day1

--- Day1/inverse_captcha.py
+++ Day1/inverse_captcha.py
@@ -1,34 +1,5 @@
 #! /usr/bin/env python3
 
-# --- Day 1: Inverse Captcha ---
-#
-# INTRODUCTION:
-#
-# The night before Christmas, one of Santa's Elves calls you in a panic.
-# "The printer's broken! We can't print the Naughty or Nice List!".
-#  By the time you make it to sub-basement 17, there are only a few minutes until midnight.
-# "We have a big problem," she says; "there must be almost fifty bugs in this system,
-# but nothing else can print The List. Stand in this square, quick!
-# There's no time to explain; if you can convince them to pay you in stars, you'll be able to--".
-# She pulls a lever and the world goes blurry.
-#
-# When your eyes can focus again, everything seems a lot more pixelated than before.
-# She must have sent you inside the computer! You check the system clock: 25 milliseconds until midnight.
-#  With that much time, you should be able to collect all fifty stars by December 25th.
-#
-# Collect stars by solving puzzles. Two puzzles will be made available on each day
-# millisecond in the advent calendar; the second puzzle is unlocked when you complete the first.
-# Each puzzle grants one star. Good luck!
-#
-# You're standing in a room with "digitization quarantine" written in LEDs along one wall.
-# The only door is locked, but it includes a small interface.
-# "Restricted Area - Strictly No Digitized Users Allowed."
-#
-# It goes on to explain that you may only leave by solving a captcha to prove you're not a human.
-# Apparently, you only get one millisecond to solve the captcha: too fast for a normal human,
-# but it feels like hours to you.
-#
-#
 # PROBLEM:
 # The captcha requires you to review a sequence of digits (your puzzle input)
 # and find the sum of all digits that match the next digit in the list.
@@ -49,15 +20,17 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser(usage="""{} """.
                                      format(sys.argv[0]),
-                                     epilog="Solves Day 1 problem of advent od code 2017")
+                                     epilog="Solves Day 1 problem of advent of code 2017")
 
     parser.add_argument("-i", "--input", type=str,
                         help="""Input file""")
+    parser.add_argument("-v", "--verbose", type=int, default=1,
+                        help="""Verbose""")
 
     return parser.parse_args()
 
 
-def main(input):
+def main(input, verbose):
 
     result = 0
 
@@ -75,7 +48,8 @@ def main(input):
 
         prev_value = value
 
-    print("Result: {}".format(result))
+    if verbose:
+        print("Result: {}".format(result))
 
 
 if __name__ == '__main__':
@@ -84,4 +58,4 @@ if __name__ == '__main__':
     with open(args.input, 'r') as f:
         input = [int(x) for x in f.readlines()[0] if x.isdigit()]
 
-    main(input)
+    main(input, args.verbose)
